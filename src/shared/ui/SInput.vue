c9c240b92cee38eb83dfbfcff59dab02483376e5
manura18 <mary.kondreskul@gmail.com>
2022-03-22 15:04:13 +0200

    feat: add UI-Input

---
+++ src/shared/ui/SInput.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps({ outline: Boolean })
+</script>
+
+<template>
+  <input
+    placeholder="Let’s create somethign dope!!! Xoxo"
+    class="py-2 w-max bg-transparent border-b-[0.1px] border-b-white focus:border-b-[#F5D040] outline-none"
+  />
+</template>
