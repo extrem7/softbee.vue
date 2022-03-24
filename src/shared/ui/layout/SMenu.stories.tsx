159a958dfb6d847db13935c572195c2753ccca5d
MadMax <sharkkizayk@gmail.com>
2022-03-24 19:16:58 +0200

    feat: add layout/menu, add svg for HomePage and fix button-color

---
+++ src/shared/ui/layout/SMenu.stories.tsx
@@ -0,0 +1,11 @@
+import type { Meta, StoryFn } from '@storybook/vue3'
+import { SMenu } from '.'
+
+const meta: Meta = {
+  title: 'UI/layout/SMenu',
+  component: SMenu,
+}
+
+export default meta
+
+export const Menu: StoryFn = (props) => <SMenu {...props} />
