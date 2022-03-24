5ecd9c724c420893fb9244666565874928f97586
manura18 <mary.kondreskul@gmail.com>
2022-03-24 19:23:52 +0200

    Feat: add arrow svg

---
+++ src/shared/ui/SArrowNext.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ArrowNext from '~/shared/assets/ArrowNext.svg'
+
+defineProps({ disabled: Boolean })
+</script>
+
+<template>
+  <img :src="ArrowNext" :class="{ 'opacity-50': disabled }" />
+</template>
