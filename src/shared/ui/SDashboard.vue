10202c0ab1610703b49aa662a66e5f3e7b5669c0
MadMax <sharkkizayk@gmail.com>
2022-03-30 17:05:51 +0300

    Feat: add listener for menu, refactoring Sbutton, SLink

--- src/shared/ui/SDashboard.vue
+++ src/shared/ui/SDashboard.vue
@@ -4,28 +4,23 @@ import PhoneMenu from '~/shared/assets/phonemenu.png'
 import { SButton } from '.'
 </script>
 <template>
-  <div
-    class="py-24 bg-[#dadada] bg-right-top bg-no-repeat"
-    style="background-image: url(src/shared/assets/bord.svg)"
-  >
-    <div class="container grid grid-cols-12 mx-auto">
-      <div class="col-start-1 col-end-5 self-center max-w-[370px]">
-        <p class="text-4xl text-[#104065]">
-          They really help to fight COVID-19 in US
-        </p>
-        <p class="my-12 text-xl text-[#3B4649]">
-          With GapNurse, facilities know they’re covered during critical moments
-          and unexpected absences without breaking your budget.
-        </p>
-        <SButton>See case study</SButton>
+  <div class="container grid grid-cols-12 mx-auto">
+    <div class="col-start-1 col-end-5 self-center max-w-[370px]">
+      <p class="text-4xl text-[#104065]">
+        They really help to fight COVID-19 in US
+      </p>
+      <p class="my-12 text-xl text-[#3B4649]">
+        With GapNurse, facilities know they’re covered during critical moments
+        and unexpected absences without breaking your budget.
+      </p>
+      <SButton>See case study</SButton>
+    </div>
+    <div class="flex col-start-6 col-end-12 justify-between">
+      <div class="flex items-end w-[48%]">
+        <img :src="Phone" class="mr-36" />
       </div>
-      <div class="flex col-start-6 col-end-12 justify-between">
-        <div class="flex items-end w-[48%]">
-          <img :src="Phone" class="mr-36" />
-        </div>
-        <div class="self-end w-[42%]">
-          <img :src="PhoneMenu" class="#" />
-        </div>
+      <div class="self-end w-[42%]">
+        <img :src="PhoneMenu" class="#" />
       </div>
     </div>
   </div>
--- src/shared/ui/SLink.vue
+++ src/shared/ui/SLink.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 defineProps({
   footer: { type: Boolean },
+  color: { type: String, default: '#104065' },
 })
 </script>
 
 <template>
   <a
     href="#"
-    :class="{ 'hover:text-[#F5D040] whitespace-nowrap': footer }"
+    :class="{ 'text-[#ffffff] hover:text-[#F5D040] whitespace-nowrap': footer }"
     class="self-center text-xl font-medium text-[#606060] hover:text-[#104065] duration-300 ease-in-out"
     ><slot
   /></a>
