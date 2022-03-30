06924f5a420e5171231b72b608f1416cd2433216
MadMax <sharkkizayk@gmail.com>
2022-03-30 19:01:27 +0300

    Feat: create (what we do) section for home

--- src/pages/HomePage/HomePage.vue
+++ src/pages/HomePage/HomePage.vue
@@ -3,6 +3,12 @@ import { SMenu, SFooter } from '~/shared/ui/layout'
 import menuImage from '~/shared/assets/menuImage.svg'
 import pcTable from '~/shared/assets/pcTable.svg'
 import { SButton, SDashboard } from '~/shared/ui'
+import code from '~/shared/assets/code.svg'
+import gears from '~/shared/assets/gears.svg'
+import build from '~/shared/assets/build.svg'
+import design from '~/shared/assets/des.svg'
+import search from '~/shared/assets/search.svg'
+import support from '~/shared/assets/support.svg'
 </script>
 
 <template>
@@ -22,7 +28,9 @@ import { SButton, SDashboard } from '~/shared/ui'
           </p>
           <SButton>Let`s talk</SButton>
         </div>
-        <div class="col-start-7 col-end-13"><img :src="menuImage" /></div>
+        <div class="col-start-6 col-end-13 mx-auto">
+          <img :src="menuImage" />
+        </div>
       </div>
     </div>
   </div>
@@ -50,8 +58,55 @@ import { SButton, SDashboard } from '~/shared/ui'
         </p>
         <SButton>Meet our team 👊</SButton>
       </div>
-      <div class="order-first col-start-1 col-end-7">
-        <img :src="pcTable" />
+      <div class="order-first col-start-1 col-end-8 self-center">
+        <img class="mx-auto" :src="pcTable" />
+      </div>
+    </div>
+  </div>
+  <div class="py-24">
+    <div class="container mx-auto">
+      <div class="py-12 mx-auto w-[83.3%] rounded-3xl shadow-2xl">
+        <div class="mx-auto w-[80%]">
+          <p class="mb-12 text-4xl font-bold text-center text-[#104065]">
+            Looking for a team willing to go the extra mile for you?
+          </p>
+        </div>
+        <div class="flex justify-around">
+          <div><img :src="gears" alt="" /></div>
+          <div>
+            <p class="text-4xl text-[#104065]">You're in the right place.</p>
+          </div>
+          <div><img :src="code" alt="" /></div>
+        </div>
+      </div>
+      <div class="mt-24 text-[#104065]">
+        <div class="mb-14"><p class="text-4xl text-center">What we do?</p></div>
+        <div class="flex justify-around">
+          <div class="flex">
+            <img :src="search" alt="" />
+            <p class="self-center ml-6 text-3xl font-bold text-[#104065]">
+              Research
+            </p>
+          </div>
+          <div class="flex">
+            <img :src="design" alt="" />
+            <p class="self-center ml-6 text-3xl font-bold text-[#104065]">
+              Design
+            </p>
+          </div>
+          <div class="flex">
+            <img :src="build" alt="" />
+            <p class="self-center ml-6 text-3xl font-bold text-[#104065]">
+              Build
+            </p>
+          </div>
+          <div class="flex">
+            <img :src="support" alt="" />
+            <p class="self-center ml-6 text-3xl font-bold text-[#104065]">
+              Support
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
