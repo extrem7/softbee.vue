f42b0f09fbda05c6bb56fb57ae39ee66bac40ecb
manura18 <mary.kondreskul@gmail.com>
2022-04-03 19:07:32 +0300

    feat: header mobile case

---
+++ src/shared/ui/layout/SHeaderMobileCase.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryFn } from '@storybook/vue3'
+import { SHeaderMobileCase } from '.'
+
+const meta: Meta = {
+  title: 'UI/HeaderMobileCase',
+  component: SHeaderMobileCase,
+  parameters: {
+    layout: 'padded',
+  },
+}
+
+export default meta
+
+export const HeaderMobileCase: StoryFn = (props) => (
+  <SHeaderMobileCase {...props} />
+)
+
+export const HeaderiPhone = HeaderMobileCase.bind({})
+HeaderMobileCase.args = { outline: true }
---
+++ src/shared/ui/layout/SHeaderMobileCase.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import HeaderPhone from '~/shared/assets/headerPhone.png'
+import HeaderiPhone from '~/shared/assets/headeriPhone.png'
+import Logo from '~/shared/assets/logo.svg'
+import AppStore from '~/shared/assets/appStore.svg'
+import GooglePlay from '~/shared/assets/googlePlay.svg'
+</script>
+<template>
+  <div class="container grid grid-cols-12 mx-auto">
+    <div class="flex col-start-1 col-end-5">
+      <div class="self-center my-6 mb-3.5">
+        <img :src="Logo" alt="img" class="mb-6" />
+        <p class="my-6 text-4xl font-semibold text-[#575195]">GapNurse App</p>
+        <p class="my-6 text-xl text-[#575195]">
+          Platform for cooperating<br />
+          nurses and healthcare<br />
+          facilities work together
+        </p>
+        <div class="flex">
+          <img :src="GooglePlay" class="mr-6" />
+          <img :src="AppStore" />
+        </div>
+      </div>
+    </div>
+    <div class="grid grid-cols-12 col-start-6 col-end-12 items-end">
+      <img :src="HeaderPhone" class="col-start-1 col-end-6" />
+      <img :src="HeaderiPhone" class="col-start-7 col-end-12" />
+    </div>
+  </div>
+</template>
